0d9895deda8dc076cefee98ba57b2a094465b05c
Keita Oouchi <keita.oouchi@gmail.com>
2012-05-15 15:57:18 +0900

    Change string formatting

--- src/seleniumwrapper/wrapper.py
+++ src/seleniumwrapper/wrapper.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def create(drivername):
     if not isinstance(drivername, str):
-        msg = "drivername should be an instance of string. given %s".format(type(drivername))
+        msg = "drivername should be an instance of string. given {0}".format(type(drivername))
         raise TypeError(msg)
     drivers = {'ie': Ie,
                'opera': Opera,
@@ -21,7 +21,7 @@ def create(drivername):
         except Exception, e:
             raise e
     else:
-        msg = "drivername should be one of [IE, Opera, Chrome, Firefox](case-insentive). given %s".format(drivername)
+        msg = "drivername should be one of [IE, Opera, Chrome, Firefox](case-insentive). given {0}".format(drivername)
         raise ValueError(msg)
 
 def _is_wrappable(obj):
@@ -49,7 +49,7 @@ class SeleniumWrapper(object):
         if _is_wrappable(driver):
             self._driver = driver
         else:
-            msg = "2nd argument should be an instance of WebDriver or WebElement. given %s.".format(type(driver))
+            msg = "2nd argument should be an instance of WebDriver or WebElement. given {0}.".format(type(driver))
             raise TypeError(msg)
 
     @property
@@ -117,19 +117,19 @@ class SeleniumWrapper(object):
 
     def href(self, partialurl=None, eager=False, timeout=10):
         if partialurl:
-            return self.xpath("//a[@contains(@href, '%s']".format(partialurl), eager, timeout)
+            return self.xpath("//a[@contains(@href, '{0}')]".format(partialurl), eager, timeout)
         return self.xpath("//a", eager, timeout)
 
     def img(self, eager=True, ext=None, timeout=10):
         if ext:
-            return self.xpath("//img[@contains(@src, '%s']".format(ext), eager, timeout)
+            return self.xpath("//img[@contains(@src, '{0}']".format(ext), eager, timeout)
         return self.xpath("//img", eager, timeout)
 
 class SeleniumContainerWrapper(object):
 
     def __init__(self, iterable):
         if not isinstance(iterable, collections.Sequence):
-            msg = "2nd argument should be an instance of collections.Sequence. given %s".format(type(iterable))
+            msg = "2nd argument should be an instance of collections.Sequence. given {0}".format(type(iterable))
             raise TypeError(msg)
         self._iterable = iterable
 
