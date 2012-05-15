b156ad4a38801837c857a1a4eb81dc65ee9f6f25
Keita Oouchi <keita.oouchi@gmail.com>
2012-05-15 22:43:21 +0900

    version up

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email='keita.oouchi@gmail.com',
     url = 'https://github.com/keitaoouchi/seleniumwrapper',
     name = 'seleniumwrapper',
-    version = '0.1.2',
+    version = '0.1.4',
     package_dir={"":"src"},
     packages = ['seleniumwrapper'],
     test_suite = "test_seleniumwrapper.suite",
