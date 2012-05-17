c8b9f87e39b67b20446e6c32a0d1fa06f7b21b8c
Keita Oouchi <keita.oouchi@gmail.com>
2012-05-17 18:29:06 +0900

    Change version to 0.1.9

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email='keita.oouchi@gmail.com',
     url = 'https://github.com/keitaoouchi/seleniumwrapper',
     name = 'seleniumwrapper',
-    version = '0.1.8',
+    version = '0.1.9',
     package_dir={"":"src"},
     packages = ['seleniumwrapper'],
     test_suite = "test_seleniumwrapper.suite",
